00162eb86eaffaa8f916a7e13746c75904b69808
jackdiscordbot5011 <37886961+jackdiscordbot5011@users.noreply.github.com>
2018-03-29 16:11:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,26 +10,38 @@ client.on('message', message => {
     	message.channel.send({embed:{
             title:"Platform Role Assignment",
             description:"React with each of the platforms you play on by clicking on the corresponding platform icon below.",
-            color: 0xFF3C3C
+            color: 0xFFF7F7
         }})
   	}
 });
 
 client.on('message', message => {
     if (message.content === '%clientconfiguremsg2') {
-    	message.channel.send('React with one or more of the following icons to assign game-based roles.');
+    	message.channel.send({embed:{
+            title:"Game-Specific Roles",
+            description:"React with one or more of the following icons to assign game-based roles.",
+            color: 0xFFF7F7
+        }})
   	}
 });
 
 client.on('message', message => {
     if (message.content === '%clientconfiguremsg3') {
-    	message.channel.send('React to the following message to receive server news.');
+    	message.channel.send({embed:{
+            title:"Server Announcements",
+            description:"React to the following message to receive server news.",
+            color: 0xFFF7F7
+        }})
   	}
 });
 
 client.on('message', message => {
     if (message.content === '%clientconfiguremsg4') {
-    	message.channel.send('React to the following message to receive news related to r/gaming.');
+    	message.channel.send({embed:{
+            title:"/r/Gaming Announcements",
+            description:"React to the following message to receive news related to r/gaming.",
+            color: 0xFFF7F7
+        }})
   	}
 });
 
