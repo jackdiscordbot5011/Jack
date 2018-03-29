9e1e44c731e1daeeb225f667df6cc3b9dabb5934
jackdiscordbot5011 <37886961+jackdiscordbot5011@users.noreply.github.com>
2018-03-28 23:28:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,27 +7,38 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.channel.send('pong');
-  	}
+    	message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
 });
 
-client.on('message', message => {
-    if (message.content === '%clientreboot') {
-    	message.channel.send('Rebooting instances...');
-  	}
-});
 
-client.on('message', message => {
-    if (message.content === '%clientsubsetup') {
-    	message.channel.send('Initializing instances...');
-  	}
-});
 
-client.on('message', message => {
-    if (message.content === '%clientfinalize') {
-    	message.channel.send('Initialization finalized.');
-  	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
