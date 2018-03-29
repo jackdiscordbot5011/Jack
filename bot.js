aa9a8b12396020526b5c22d5b1c4ad975ded5c1d
jackdiscordbot5011 <37886961+jackdiscordbot5011@users.noreply.github.com>
2018-03-28 23:38:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,24 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '%clientconfiguremsg2') {
+    	message.channel.send('React with one or more of the following icons to assign game-based roles.');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '%clientconfiguremsg3') {
+    	message.channel.send('React to the following message to receive server news.');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '%clientconfiguremsg4') {
+    	message.channel.send('React to the following message to receive news related to r/gaming.');
+  	}
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
