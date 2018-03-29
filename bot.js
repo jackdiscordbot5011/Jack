6ac0732931b2cd4f462d243fea8df1f483304867
jackdiscordbot5011 <37886961+jackdiscordbot5011@users.noreply.github.com>
2018-03-28 23:00:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,23 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '%clientreboot') {
+    	message.channel.send('Rebooting instances...');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '%clientsubsetup') {
+    	message.channel.send('Initializing instances...');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '%clientfinalize') {
+    	message.channel.send('Initialization finalized.');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
