b4a2141214451426fa8cf1ac5bb680b8790f81f4
jackdiscordbot5011 <37886961+jackdiscordbot5011@users.noreply.github.com>
2018-03-30 22:17:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 
-client.on('ready', function() {
-    bot.user.setUsername("AppleCare");
-});
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
