e1867b72725408d454af0bc0c5d1d7b929cb72bf
jackdiscordbot5011 <37886961+jackdiscordbot5011@users.noreply.github.com>
2018-03-30 20:22:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ client.on("message", async message => {
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
     if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+      return message.channel.send("Permission denied.");
     
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
@@ -106,17 +106,17 @@ client.on("message", async message => {
     if(!member)
       return message.reply("Please mention a valid member of this server");
     if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      return message.channel.send("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
     
     // slice(1) removes the first part, which here should be the user mention!
     let reason = args.slice(1).join(' ');
     if(!reason)
-      return message.reply("Please indicate a reason for the kick!");
+      return message.channel.send("Indicate valid reason.");
     
     // Now, time for a swift kick in the nuts!
     await member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    message.channel.send(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
   }
   
