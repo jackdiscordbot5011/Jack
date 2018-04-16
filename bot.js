babbb42bd4a9a5c00f6fa9a392991ca996867b4a
jackdiscordbot5011 <37886961+jackdiscordbot5011@users.noreply.github.com>
2018-04-15 22:05:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 bot.on('ready', function() {
-    bot.user.setUsername("MyNewUsername");
+    bot.user.setUsername("iOS");
 }
 client.login(process.env.BOT_TOKEN);
